d3936be927283e22abdcdc7bc196a48c6fd3e333
Ramesh Ayyagari <rayyagari2@gmail.com>
2026-01-31 12:29:35 -0500

    Fix CORS to allow Vercel UI

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ def health():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001",
+        allow_origins=[
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
+    "https://compass-ui-blush.vercel.app",
+],
+
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
